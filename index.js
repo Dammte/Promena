c80e9dafeeb645a31629c994772ed707bb3137a5
Everhech <edwin415@outlook.com>
2024-10-06 16:06:32 -0500

    Update frontend and general functionality

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 /**
  * @description Scroll fast when the view is on an specific part of the page.
  */
-document.addEventListener("scroll", function () {
-  const backToTopButton = document.querySelector(".back-to-top");
-  if (window.scrollY > 300) {
-    backToTopButton.style.display = "block";
-  } else {
-    backToTopButton.style.display = "none";
+// document.addEventListener("scroll", function () {
+//   const backToTopButton = document.querySelector(".back-to-top");
+//   if (window.scrollY > 2000) {
+//     backToTopButton.style.display = "block";
+//   } else {
+//     backToTopButton.style.display = "none";
+//   }
+// });
+const backToTopButton = document.querySelector(".back-to-top");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 1000){
+    backToTopButton.classList.add("active");
+  }else {
+    backToTopButton.classList.remove("active");
   }
 });
 
@@ -20,7 +29,7 @@ function sendEmail() {
     email: document.getElementById("Email").value,
     message: document.getElementById("Message").value,
   };
-  emailjs.send("service_elud2d5", "template_0y1xruk", params).then(
+  emailjs.send("service_elud2d20", "template_0y2xruk", params).then(
     (response) => {
       alert("¡Mensaje enviado! " + response.status);
       console.log("success!", response.status, response.text);
@@ -48,4 +57,28 @@ const observer = new IntersectionObserver((entries) => {
 });
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((element) => observer.observe(element));
+
+
+
+const observerSectionWhy = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if(entry.isIntersecting){
+      entry.target.classList.add('up');
+    } else {
+      entry.target.classList.remove('up');
+    }
+  });
+});
+
+const goSectionWhy = document.querySelectorAll('.down');
+goSectionWhy.forEach((element) => observerSectionWhy.observe(element));
+
+
+function rotateCard(id) {
+  const card = document.getElementById(id);
+  card.classList.toggle('is-flipped');
+  card.classList.toggle('fitContent');
+}
+
+
 
